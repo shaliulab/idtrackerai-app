31bb9bb74b3ad39af5f5a4d5b14cb242044d3761
Ricardo Ribeiro <ricardojvr@gmail.com>
2019-01-31 13:38:10 +0000

    Load list_of_blobs

--- idtrackerai_gui/helpers.py
+++ idtrackerai_gui/helpers.py
@@ -1,4 +1,6 @@
+from idtrackerai.list_of_blobs import ListOfBlobs
 from idtrackerai.video import Video
+import os
 
 class Chosen_Video(object):
     
@@ -6,6 +8,11 @@ class Chosen_Video(object):
         self.video = kwargs.get('video',  Video() )
         self.list_of_fragments = kwargs.get('list_of_fragments', None)
         self.list_of_global_fragments = kwargs.get('list_of_global_fragments', None)
+
+        if self.video.blobs_path is not None and os.path.exists(self.video.blobs_path):
+            self.list_of_blobs = ListOfBlobs.load(self.video, self.video.blobs_path)
+        else:
+            self.list_of_blobs = None
 
         self.chosen = 'Default String'
         self.processes_list = processes_list
