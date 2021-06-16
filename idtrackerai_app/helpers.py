6f0083bbbba774d952aca997730af7b26643e955
Paco Romero Ferrero <paco.romero.ferrero@gmail.com>
2021-06-16 12:33:38 +0200

    to black

--- idtrackerai_app/helpers.py
+++ idtrackerai_app/helpers.py
@@ -2,23 +2,28 @@ from idtrackerai.list_of_blobs import ListOfBlobs
 from idtrackerai.video import Video
 import os
 
-class Chosen_Video(object):
-    
-    def __init__(self, processes_list = None, **kwargs):
-        self.video = kwargs.get('video',  Video() )
-        self.list_of_fragments = kwargs.get('list_of_fragments', None)
-        self.list_of_global_fragments = kwargs.get('list_of_global_fragments', None)
 
-        if self.video.blobs_path is not None and os.path.exists(self.video.blobs_path):
-            self.list_of_blobs = ListOfBlobs.load(self.video, self.video.blobs_path)
+class Chosen_Video(object):
+    def __init__(self, processes_list=None, **kwargs):
+        self.video = kwargs.get("video", Video())
+        self.list_of_fragments = kwargs.get("list_of_fragments", None)
+        self.list_of_global_fragments = kwargs.get(
+            "list_of_global_fragments", None
+        )
+
+        if self.video.blobs_path is not None and os.path.exists(
+            self.video.blobs_path
+        ):
+            self.list_of_blobs = ListOfBlobs.load(
+                self.video, self.video.blobs_path
+            )
         else:
             self.list_of_blobs = None
 
-        self.chosen = 'Default String'
+        self.chosen = "Default String"
         self.processes_list = processes_list
         self.processes_to_restore = {}
         self.old_video = None
-        
 
     def set_chosen_item(self, chosen_string):
         self.chosen = chosen_string
