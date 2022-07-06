429dc83f34ecaacc4afb3d50c2ac21b0ea762da9
antortjim <ntoniohu@gmail.com>
2022-07-06 18:01:14 +0000

    Import local_settings if exists into the package

--- idtrackerai_app/__init__.py
+++ idtrackerai_app/__init__.py
@@ -4,3 +4,14 @@ import locale
 
 locale.setlocale(locale.LC_ALL, "en_US.UTF-8")
 locale.setlocale(locale.LC_NUMERIC, "en_US.UTF-8")
+
+# TODO
+# Is this actually needed?
+# Or is there a code in pyforms that always imports the local_settings?
+from confapp import conf
+
+try:
+    import local_settings
+    conf += local_settings
+except:
+    pass
