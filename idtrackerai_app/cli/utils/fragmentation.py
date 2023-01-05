7459ab69643163b1e00297dd8fbeebaec57363b7
antortjim <ntoniohu@gmail.com>
2023-01-05 14:57:01 +0100

    Implement utils

---
+++ idtrackerai_app/cli/utils/fragmentation.py
@@ -0,0 +1,37 @@
+import argparse
+import os.path
+import numpy as np
+
+
+def get_parser():
+
+    ap = argparse.ArgumentParser()
+    ap.add_argument("--chunk", type=int, required=True)
+    return ap
+
+
+def main():
+    ap = get_parser()
+    args = ap.parse_args()
+    chunk = args.chunk
+    fragment_file = os.path.join(f"session_{str(chunk).zfill(6)}", "preprocessing", "fragments.npy")
+    assert os.path.exists(fragment_file), f"{fragment_file} not found"
+    list_of_fragments = np.load(fragment_file, allow_pickle=True).item()
+    for fragment in list_of_fragments.fragments:
+        print(fragment.start_end)
+
+
+def load_list_of_blobs():
+    ap = get_parser()
+    args = ap.parse_args()
+    chunk = args.chunk
+    blobs_collection_file = os.path.join(f"session_{str(chunk).zfill(6)}", "preprocessing", "blobs_collection.npy")
+    assert os.path.exists(blobs_collection_file), f"{blobs_collection_file} not found"
+    list_of_blobs = np.load(blobs_collection_file, allow_pickle=True).item()
+    import ipdb; ipdb.set_trace()
+
+
+
+
+if __name__ == "__main__":
+    main()
