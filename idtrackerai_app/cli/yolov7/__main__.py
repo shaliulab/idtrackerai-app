4c7429938004543f8c44f54d8df9d5711eec1455
Antonio Ortega <ntoniohu@gmail.com>
2023-02-04 17:50:17 +0100

    Add docstring

--- idtrackerai_app/cli/yolov7/__main__.py
+++ idtrackerai_app/cli/yolov7/__main__.py
@@ -3,6 +3,7 @@ import os.path
 import glob
 import joblib
 import logging
+import pickle
 import matplotlib.pyplot as plt
 import numpy as np
 import cv2
@@ -35,39 +36,67 @@ def main():
 
     ap = get_parser()
     args = ap.parse_args()
-    integrate_yolov7(store_path=args.store_path, n_jobs=n_jobs, input=input, output=args.output, chunks=args.chunks)
+    integrate_yolov7(store_path=args.store_path, n_jobs=args.n_jobs, input=input, output=args.output, chunks=args.chunks)
 
 
 def integrate_yolov7(store_path, n_jobs, input, output, chunks):
     """
-    Assumes all problematic frames have a label in the yolov7 repo
+    Call process_chunk in parallel
+    See also: process_chunk
     """
 
     assert os.path.exists(input), f"{input} not found (wd: {os.getcwd()}"
     assert os.path.exists(output), f"{output} not found"
     assert os.path.isdir(output), f"{output} is not a directory"
 
-    filename = f"session_{str(chunk).zfill(6)}_integration_output.txt"
-    logfile = os.path.join(output, filename)
-    assert os.path.exists(logfile), f"{logfile} does not exist, did you run the YOLOv7 integration step already?"
 
-    allowed_classes={0: "fly"}
+    allowed_classes={0: "fly", 1: "blurry"}
     store = validate_store(store_path)
-
-    count = 0
-    original_output = output
-
-
-
-   
     Output = joblib.Parallel(n_jobs=n_jobs)(joblib.delayed(process_chunk)(
-            store_path, chunk, input, allowed_classes=allowed_classes, logfile=logfile
+            store_path, chunk, input, output, allowed_classes=allowed_classes,
         )
         for chunk in chunks
     )
 
 
-def process_chunk(store_path, chunk, input, allowed_classes=None, logfile=None):
+def process_chunk(store_path, chunk, input, output, allowed_classes=None, logfile=None):
+    """
+    Improve the idtrackerai preprocessing results by collecting the output of an external AI system
+
+    This function
+
+       1. Lists the available labels produced by the AI system and stored under the input folder
+       2. Incorporates them into an existing blobs_collection (list_of_blobs) produced by idtrackerai
+       3. Produces a log of the results
+           1. in the idtrackerai_folder in plain text format with basic stats (total # frames, # successful, # not succesful)
+           2. in the preprocessing folder in pickle format (ai.pkl) a dictionary with the succesful and unsuccessful frame numbers
+
+    Arguments:
+    
+        * store_path (str): Path to a metadata.yaml
+        * chunk (int): Chunk of the imgstore linked to the metadata to be processed
+        * input (str): Folder containing labels produced by the AI system
+        * output (str): Folder on which to store logs of the process
+        * allowed_class (dict): If passed, only classes within the contained keys will be used, the rest will be ignored
+        If None, all are used
+    
+    Returns:
+        * chunk (int): Passed chunk
+        * sucess_rate (float): Fraction of frames processed by the AI system that meet the success criteria
+    
+    See annotate_chunk_with_yolov7 for details
+
+    """
+
+    basedir = os.path.dirname(store_path)
+
+    filename = f"session_{str(chunk).zfill(6)}_integration_output.txt"
+    if logfile is None:
+        logfile = os.path.join(output, filename)
+
+    if os.path.exists(logfile):
+        os.remove(logfile)
+
 
     regex=os.path.join(input, f"*_{chunk}-*.txt")
     labels = sorted(glob.glob(regex))
@@ -96,11 +125,11 @@ def process_chunk(store_path, chunk, input, allowed_classes=None, logfile=None):
             filehandle.write(f"Processed frames: {processed_successfully}\n")
             filehandle.write(f"Total frames: {len(frames)}\n")
             filehandle.write(f"Success rate: {success_rate}\n")
-            for frame_number in successful_frames:
-                filehandle.write(f"Success integration: {frame_number}\n")
-            for frame_number in failed_frames:
-                filehandle.write(f"Fail integration: {frame_number}\n")
 
+    pkl_file = os.path.join(basedir, "idtrackerai", f"session_{str(chunk).zfill(6)}", "preprocessing", "ai.pkl")
+    with open(pkl_file, "wb") as filehandle:
+        pickle.dump({"success": successful_frames, "fail": failed_frames}, pkl_file)
+    
     return chunk, success_rate
 
 if __name__ == "__main__":
