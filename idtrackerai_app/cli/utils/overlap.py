141cfdeda64ae16288a18ae8a59fe717a312145f
Antonio Ortega <ntoniohu@gmail.com>
2023-02-12 18:18:08 +0100

    Update overlap module

--- idtrackerai_app/cli/utils/overlap.py
+++ idtrackerai_app/cli/utils/overlap.py
@@ -1,6 +1,17 @@
+import argparse
+import os.path
+
+import joblib
+
 from idtrackerai.list_of_blobs.overlap import compute_overlapping_between_two_subsequent_frames
 from idtrackerai.list_of_blobs import ListOfBlobs
 
+def get_parser():
+
+    ap = argparse.ArgumentParser()
+    ap.add_argument("--store-path", type=str, help="path to metadata.yaml")
+    ap.add_argument("--chunks", type=int, nargs="+")
+    return ap
 
 def main():
 
@@ -8,7 +19,7 @@ def main():
     ap = get_parser()
     args = ap.parse_args()
 
-    if args.chunks is None
+    if args.chunks is None:
         raise NotImplementedError
     else:
         chunks = args.chunks
@@ -20,7 +31,7 @@ def main():
     )
 
 
-def process_chunk(store_path, chunk)
+def process_chunk(store_path, chunk):
 
     list_of_blobs_path = os.path.join(os.path.dirname(store_path), "idtrackerai", f"session_{str(chunk).zfill(6)}", "preprocessing", "blobs_collection.npy")
     list_of_blobs_path_next = os.path.join(os.path.dirname(store_path), "idtrackerai", f"session_{str(chunk+1).zfill(6)}", "preprocessing", "blobs_collection.npy")
@@ -30,4 +41,13 @@ def process_chunk(store_path, chunk)
         list_of_blobs=ListOfBlobs.load(list_of_blobs_path)
         list_of_blobs_next=ListOfBlobs.load(list_of_blobs_path_next)
 
-        compute_overlapping_between_two_subsequent_frames(list_of_blobs.blobs_in_video[-1], list_of_blobs_next.blobs_in_video[0]
+        overlap_pattern=compute_overlapping_between_two_subsequent_frames(list_of_blobs.blobs_in_video[-1], list_of_blobs_next.blobs_in_video[0], queue=None, do=False)
+        pattern=[]
+        for ((blob_before, _), (blob_after, _)) in overlap_pattern:
+            
+            pattern.append((blob_before.in_frame_index, blob_after.in_frame_index))
+        
+        return pattern
+
+    else:
+        return []
