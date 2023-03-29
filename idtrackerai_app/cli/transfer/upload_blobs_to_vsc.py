44c03001542c4d8290c3c09ecc72c38c6d72e6a4
antortjim <ntoniohu@gmail.com>
2023-03-29 14:24:25 +0200

    Update transfer

--- idtrackerai_app/cli/transfer/upload_blobs_to_vsc.py
+++ idtrackerai_app/cli/transfer/upload_blobs_to_vsc.py
@@ -13,6 +13,7 @@ REMOTE_VIDEOS="/staging/leuven/stg_00115/Data/flyhostel_data/videos"
 def get_parser():
     ap = argparse.ArgumentParser()
     ap.add_argument("--experiment", required=True)
+    ap.add_argument("-n", "--dry-run", default=False, action="store_true", dest="dry_run")
     # ap.add_argument("--chunks", type=int, nargs="+")
     return ap
 
@@ -31,21 +32,22 @@ def main():
     )
     if os.path.exists(human_corrections_file):
 
-        corrections = pd.read_csv(human_corrections_file)
+        corrections = pd.read_csv(human_corrections_file, comment="#")
         chunks = sorted(list(set(corrections["chunk"].tolist())))
-        upload_to_vsc(args.experiment, chunks)
+        upload_to_vsc(args.experiment, chunks, args.dry_run)
 
     else:
         print(f"{human_corrections_file} not found")
 
 
-def upload_to_vsc(experiment, chunks):
+def upload_to_vsc(experiment, chunks, dry_run=False):
     
     for chunk in chunks:
         blobs_collection=get_blobs_collection(FLYHOSTEL_VIDEOS, experiment, chunk)
         remote_blobs_collection=get_blobs_collection(REMOTE_VIDEOS, experiment, chunk)
         print(f"{blobs_collection} -> login:{remote_blobs_collection}")
-        transfer_file(blobs_collection, "login:" + remote_blobs_collection, update=True)
+        if not dry_run:
+            transfer_file(blobs_collection, "login:" + remote_blobs_collection, update=True)
 
 
 if __name__ == "__main__":
